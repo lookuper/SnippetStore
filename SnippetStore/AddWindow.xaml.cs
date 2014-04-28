6c66258382eb37444c426d13f737cdc0c0904ea3
Maksym Chernenko <Maksym.Chernenko@gmail.com>
2014-04-28 23:09:29 +0300

    add new window

---
+++ SnippetStore/AddWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnippetStore
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = Box.Text;
+            DialogResult = true;
+
+            Close();
+        }
+    }
+}
